c20c48e18a22d51a748afb68bf27e5d4ee0bd610
therazus <74174794+therazus@users.noreply.github.com>
2023-10-26 02:48:03 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const ipAddress = "https://4ded-212-104-225-211.ngrok.io/api/v1";
+const ipAddress =
+  "https://6adb-2402-4000-b280-8b4c-2db1-2fb-4b45-9424.ngrok.io/api/v1";
 
 // https://hotel-mis-test-bbb375a19c7a.herokuapp.com/
 
