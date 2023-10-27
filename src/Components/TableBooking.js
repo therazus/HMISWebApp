9ee1697af98a38a51fbbee1b31785c07d7940d3a
therazus <74174794+therazus@users.noreply.github.com>
2023-10-27 23:32:17 +0530

    Finish

--- src/Components/TableBooking.js
+++ src/Components/TableBooking.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Form, Row, Col, Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Form, Row, Col, Container, Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 import "../Styles/tablebooking.css";
 
@@ -17,23 +18,52 @@ import { ThemeProvider } from "@mui/material/styles";
 import { themeTyp } from "../Styles/Theme";
 
 import TableMap from "../Components/TableMap";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 // =================================================================================
 
 const SwitchFormComponent = () => {
+  const navigate = useNavigate();
+  const [disReserve, setDisReserve] = useState(true);
   const [stayInHotel, setStayInHotel] = useState(false);
   const [roomNumber, setRoomNumber] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [telephone, setTelephone] = useState("");
-  const [meal, setMeal] = useState("");
-  const [time, setTime] = React.useState(dayjs("2022-04-17T15:30"));
+
+  const [showErrPhone, setShowErrPhone] = useState(false);
+
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  // const [meal, setMeal] = useState("");
+  // const [time, setTime] = React.useState(dayjs());
+
+  useEffect(() => {
+    if (
+      (roomNumber !== "" && password !== "") |
+        (name !== "" && telephone !== "") &&
+      date !== "" &&
+      time !== ""
+    ) {
+      setDisReserve(false);
+    } else {
+      setDisReserve(true);
+    }
+  }, [roomNumber, password, name, telephone, date, time]);
+
+  const isValidPhoneNumber = (phoneNo) => {
+    // Regular expression for mobile phone number validation
+    const phoneRegex = /^(?:\+94[1-9]\d{8}|0\d{9}|[1-9]\d{8})$/;
+    return phoneRegex.test(phoneNo);
+  };
 
   const handleSwitchChange = () => {
     setStayInHotel(!stayInHotel);
   };
 
   const handleSubmit = (e) => {
+    setDisReserve(true);
+
     e.preventDefault();
     if (stayInHotel) {
       // Handle log in form submission
@@ -42,35 +72,45 @@ const SwitchFormComponent = () => {
       // Handle sign up form submission
       console.log("Rserving for Outsider:", name, telephone);
     }
+
+    alert("Your Table is Booked!");
+    wait(1500);
+    navigate("/");
   };
 
   const handleBooking = (tableNumber) => {
     console.log(`Table ${tableNumber} booked!`);
     // Here you can implement further booking logic or API calls
   };
+
+  const handleTelephoneChange = (e) => {
+    setTelephone(e.target.value);
+    setShowErrPhone(isValidPhoneNumber(e));
+  };
+
   // =================================================================================
 
-  const getMinTime = () => {
-    if (meal === "Breakfast") {
-      return dayjs().set("hour", 8).set("minute", 0);
-    } else if (meal === "Lunch") {
-      return dayjs().set("hour", 12).set("minute", 0);
-    } else if (meal === "Dinner") {
-      return dayjs().set("hour", 19).set("minute", 0);
-    }
-    return dayjs();
-  };
+  // const getMinTime = () => {
+  //   if (meal === "Breakfast") {
+  //     return dayjs().set("hour", 8).set("minute", 0);
+  //   } else if (meal === "Lunch") {
+  //     return dayjs().set("hour", 12).set("minute", 0);
+  //   } else if (meal === "Dinner") {
+  //     return dayjs().set("hour", 19).set("minute", 0);
+  //   }
+  //   return dayjs();
+  // };
 
-  const getMaxTime = () => {
-    if (meal === "Breakfast") {
-      return dayjs().set("hour", 10).set("minute", 0);
-    } else if (meal === "Lunch") {
-      return dayjs().set("hour", 14).set("minute", 30);
-    } else if (meal === "Dinner") {
-      return dayjs().set("hour", 21).set("minute", 0);
-    }
-    return dayjs();
-  };
+  // const getMaxTime = () => {
+  //   if (meal === "Breakfast") {
+  //     return dayjs().set("hour", 10).set("minute", 0);
+  //   } else if (meal === "Lunch") {
+  //     return dayjs().set("hour", 14).set("minute", 30);
+  //   } else if (meal === "Dinner") {
+  //     return dayjs().set("hour", 21).set("minute", 0);
+  //   }
+  //   return dayjs();
+  // };
 
   // =================================================================================
 
@@ -142,13 +182,50 @@ const SwitchFormComponent = () => {
                         <Form.Control
                           type="tel"
                           value={telephone}
-                          onChange={(e) => setTelephone(e.target.value)}
+                          onChange={handleTelephoneChange}
                         />
+                        {showErrPhone && (
+                          <div style={{ display: "inline-block" }}>
+                            <Alert
+                              variant="danger"
+                              style={{
+                                fontSize: 10,
+                                height: "8px",
+                                marginTop: "5px",
+                                display: "flex", // Use flexbox
+                                alignItems: "center", // Center vertically
+                                justifyContent: "center",
+                                marginBottom: "-4px",
+                              }}
+                            >
+                              Please enter a valid Mobile Number
+                            </Alert>
+                          </div>
+                        )}
                       </Form.Group>
                     </>
                   )}
 
                   <Form.Group>
+                    <Form.Label className="form-lable" controlId="name">
+                      <Typography variant="forum">Date</Typography>
+                    </Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={date}
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                    <Form.Label className="form-lable" controlId="Time">
+                      <Typography variant="forum">Time</Typography>
+                    </Form.Label>
+                    <Form.Control
+                      type="time"
+                      value={time}
+                      onChange={(e) => setTime(e.target.value)}
+                    />
+                  </Form.Group>
+
+                  {/* <Form.Group>
                     <Form.Label className="form-lable" controlId="meal">
                       <Typography variant="forum">Meal</Typography>
                     </Form.Label>
@@ -182,8 +259,12 @@ const SwitchFormComponent = () => {
                         </DemoItem>
                       </DemoContainer>
                     </LocalizationProvider>
-                  </Form.Group>
-                  <button className="btnbooking" onClick={handleSubmit}>
+                  </Form.Group> */}
+                  <button
+                    className="btnbooking"
+                    onClick={handleSubmit}
+                    disabled={disReserve}
+                  >
                     Book Table
                   </button>
                 </Form>
